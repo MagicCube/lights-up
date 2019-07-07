7d156e2aad9bd16d3a5acc8b54171e7c9ba9c205
Henry Li <henry1943@163.com>
2019-07-07 20:40:09 +0800

    Use HSV color instead of RGB

--- lights-up-electron/src/web/components/App/index.tsx
+++ lights-up-electron/src/web/components/App/index.tsx
@@ -2,7 +2,7 @@ import Switch from 'antd/es/switch';
 import cn from 'classnames';
 import * as React from 'react';
 
-import { RGB } from '../../types/Colors';
+import { HSV } from '../../types/Colors';
 
 import { AppHeader } from '../AppHeader';
 import { ColorPicker } from '../ColorPicker';
@@ -10,21 +10,21 @@ import { ColorPicker } from '../ColorPicker';
 import * as styles from './index.less';
 
 export const App: React.SFC = () => {
-  const [color, setColor] = React.useState({ r: 255, g: 255, b: 255 });
-  const [brightness, setBrightness] = React.useState(255);
+  const [hsvColor, setHSVColor] = React.useState({ h: 60, s: 100, v: 0 });
+  const [brightness, setBrightness] = React.useState(100);
   const [power, setPower] = React.useState(false);
 
   const handlePowerChange = React.useCallback((checked: boolean) => {
     setPower(checked);
   }, []);
 
-  const handleColorChange = React.useCallback((newColor: RGB) => {
-    setColor(newColor);
+  const handleColorChange = React.useCallback((newColor: HSV) => {
+    setHSVColor(newColor);
   }, []);
   const handleBrightnessChange = React.useCallback((newBrightess: number) => {
     setBrightness(newBrightess);
   }, []);
-  console.info(power, color, brightness);
+  console.info(power, hsvColor, brightness);
   return (
     <div>
       <AppHeader>
@@ -33,7 +33,12 @@ export const App: React.SFC = () => {
       <main className={cn({ [styles.main]: true, [styles.disabled]: !power })}>
         <div className={styles.overlay} />
         <section>
-          <ColorPicker onColorChange={handleColorChange} onBrightnessChange={handleBrightnessChange} />
+          <ColorPicker
+            hsvColor={hsvColor}
+            brightness={brightness}
+            onColorChange={handleColorChange}
+            onBrightnessChange={handleBrightnessChange}
+          />
         </section>
         <section />
       </main>
--- lights-up-electron/src/web/components/ColorPicker/index.tsx
+++ lights-up-electron/src/web/components/ColorPicker/index.tsx
@@ -2,30 +2,32 @@
 import iro from '@jaames/iro';
 import * as React from 'react';
 
-import { RGB } from '../../types/Colors';
+import { HSV } from '../../types/Colors';
 
 import * as styles from './index.less';
 
 interface ColorPickerProps {
-  onColorChange?: (color: RGB) => void;
+  hsvColor: HSV;
+  brightness: number;
+  onColorChange?: (color: HSV) => void;
   onBrightnessChange?: (brightness: number) => void;
 }
 
 export const ColorPicker: React.SFC<ColorPickerProps> = (props: ColorPickerProps) => {
-  const { onColorChange, onBrightnessChange } = props;
+  const { hsvColor, brightness, onColorChange, onBrightnessChange } = props;
   const rootRef = React.useRef(null);
   const colorPickerRef = React.useRef(null);
   const handleColorChange = React.useCallback(
-    (color, changes) => {
+    (newColor, changes) => {
       if (changes.v) {
+        // If only brightness has been changed
         if (typeof onColorChange === 'function') {
           // Use v as brightness.
-          onBrightnessChange(Math.round((color.hsv.v / 100) * 255));
+          onBrightnessChange(newColor.hsv.v);
         }
       } else if (typeof onColorChange === 'function') {
         // Only use h and s to construct the color.
-        const newColor = new iro.Color({ h: color.hsv.h, s: color.hsv.s, v: 100 });
-        onColorChange(newColor.rgb);
+        onColorChange({ h: newColor.hsv.h, s: newColor.hsv.s, v: 100 });
       }
     },
     [onColorChange, onBrightnessChange]
@@ -43,6 +45,24 @@ export const ColorPicker: React.SFC<ColorPickerProps> = (props: ColorPickerProps
       colorPickerRef.current = null;
     };
   }, [rootRef]);
+  React.useEffect(() => {
+    if (colorPickerRef.current) {
+      const currentOfColorPicker = colorPickerRef.current.color;
+      if (
+        hsvColor.h !== currentOfColorPicker.hsv.h ||
+        hsvColor.s !== currentOfColorPicker.hsv.s ||
+        brightness !== currentOfColorPicker.hsv.v
+      ) {
+        const newHSVColor = {
+          h: hsvColor.h,
+          s: hsvColor.s,
+          v: 100
+        };
+        console.info('Set to ', newHSVColor);
+        colorPickerRef.current.color.hsv = newHSVColor;
+      }
+    }
+  }, [hsvColor, brightness]);
   // Binding events.
   React.useEffect(() => {
     if (colorPickerRef.current) {
