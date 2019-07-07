c880e5eb779951cd9216cbcb9dd62fa2535f2733
Henry Li <henry1943@163.com>
2019-07-07 18:13:49 +0800

    Implement

--- lights-up-electron/src/web/components/App/index.tsx
+++ lights-up-electron/src/web/components/App/index.tsx
@@ -1,21 +1,40 @@
-import * as React from 'react';
 import Switch from 'antd/es/switch';
+import cn from 'classnames';
+import * as React from 'react';
+
+import { RGB } from '../../types/Colors';
+
+import { AppHeader } from '../AppHeader';
+import { ColorPicker } from '../ColorPicker';
 
 import * as styles from './index.less';
 
 export const App: React.SFC = () => {
+  const [color, setColor] = React.useState({ r: 255, g: 255, b: 255 });
+  const [brightness, setBrightness] = React.useState(255);
+  const [power, setPower] = React.useState(false);
+
+  const handlePowerChange = React.useCallback((checked: boolean) => {
+    setPower(checked);
+  }, []);
+
+  const handleColorChange = React.useCallback((newColor: RGB) => {
+    setColor(newColor);
+  }, []);
+  const handleBrightnessChange = React.useCallback((newBrightess: number) => {
+    setBrightness(newBrightess);
+  }, []);
+  console.info(power, color, brightness);
   return (
     <div>
-      <main>
-        <header className={styles.header}>
-          <div className={styles.left}>
-            <h1>MagicCube Lights-up</h1>
-          </div>
-          <div className={styles.right}>
-            <Switch defaultChecked />
-          </div>
-        </header>
-        <section />
+      <AppHeader>
+        <Switch checkedChildren="开" unCheckedChildren="关" checked={power} onChange={handlePowerChange} />
+      </AppHeader>
+      <main className={cn({ [styles.main]: true, [styles.disabled]: !power })}>
+        <div className={styles.overlay} />
+        <section>
+          <ColorPicker onColorChange={handleColorChange} onBrightnessChange={handleBrightnessChange} />
+        </section>
         <section />
       </main>
     </div>
