ff2ea4416478487ef4c0c4d4c9e12e49031b947d
亨睿 <henry.lx@alibaba-inc.com>
2019-07-08 11:08:59 +0800

    Add dummy

---
+++ lights-up-electron/src/web/messaging/dummy.ts
@@ -0,0 +1,6 @@
+import { Message, MessageContent } from '../../common/Message';
+
+export function postMessage(type: string, content?: MessageContent) {
+  const message: Message = { type, ...content };
+  console.info('[DUMMY]', message);
+}
