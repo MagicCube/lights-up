41ca3c0e30dfb28c441599d88fcac0917efef2b3
亨睿 <henry.lx@alibaba-inc.com>
2019-07-08 20:49:01 +0800

    Rewrite

--- lights-up-esp8266/src/main.cpp
+++ lights-up-esp8266/src/main.cpp
@@ -1,25 +1,43 @@
 #include <Arduino.h>
+#include <ArduinoOTA.h>
 
-#include <FastLED.h>
+#include <ESP8266mDNS.h>
+#include <ESP8266WebServer.h>
+#include <ESP8266WiFi.h>
 
-#define NUM_LEDS 30
-#define DATA_PIN D3
+ESP8266WebServer server;
 
-CRGB leds[NUM_LEDS];
+void handleTest() {
+  server.send(200, "application/json", "{\"name\":\"" + server.arg("name") + "\"}");
+}
 
-void initFastLED() {
-  FastLED.addLeds<WS2812B, DATA_PIN, GRB>(leds, NUM_LEDS);
-  FastLED.setBrightness(255);
-  for (int i = 0; i < NUM_LEDS; i++) {
-    leds[i] = CRGB::OrangeRed;
-  }
-  FastLED.show();
+void handleTurnOn() {
+  digitalWrite(LED_BUILTIN, LOW);
+  server.send(200, "text/plain", "OK");
+}
+
+void handleTurnOff() {
+  digitalWrite(LED_BUILTIN, HIGH);
+  server.send(200, "text/plain", "OK");
 }
 
 void setup() {
-  Serial.begin(115200);
-  initFastLED();
+  pinMode(LED_BUILTIN, OUTPUT);
+  digitalWrite(LED_BUILTIN, HIGH);
+  WiFi.begin("iPhone", "13913954971");
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(100);
+  }
+  MDNS.begin("ledstrip-01");
+  ArduinoOTA.begin();
+  server.on("/test", handleTest);
+  server.on("/turnOn", handleTurnOn);
+  server.on("/turnOff", handleTurnOff);
+  server.begin();
 }
 
 void loop() {
+  ArduinoOTA.handle();
+  server.handleClient();
+  MDNS.update();
 }
