ee033ac9f8cc5c1bed95d7c227629b0602f1902a
亨睿 <henry.lx@alibaba-inc.com>
2019-07-08 10:29:03 +0800

    Make messaging as directory

--- lights-up-electron/src/web/messaging/index.ts
+++ lights-up-electron/src/web/messaging/index.ts
@@ -1,6 +1,6 @@
 import { ipcRenderer } from 'electron';
 
-import { Message, MessageContent } from '../common/Message';
+import { Message, MessageContent } from '../../common/Message';
 
 export function postMessage(type: string, content?: MessageContent) {
   const message: Message = { type, ...content };
