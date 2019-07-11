151f5101dac73fc5ed0e6a7a7e2f0db8be0250b4
Henry Li <henry1943@163.com>
2019-07-11 22:50:17 +0800

    Remove BUILTIN_LED

--- lights-up-esp8266/src/devices/LEDStrip.cpp
+++ lights-up-esp8266/src/devices/LEDStrip.cpp
@@ -34,8 +34,6 @@ void LEDStrip::setHSVColor(HSV hsv, bool immediately) {
 }
 
 void LEDStrip::begin() {
-  pinMode(LED_BUILTIN, OUTPUT);
-  digitalWrite(LED_BUILTIN, HIGH);
   FastLED.addLeds<WS2812B, LED_STRIP_ROW_1_PIN, GRB>(_row1, LED_STRIP_ROW_1_LENGTH);
   setBrightness(_brightness, false);
   powerOn();
@@ -48,12 +46,10 @@ void LEDStrip::update() {
 
 void LEDStrip::powerOn() {
   _isOn = true;
-  digitalWrite(LED_BUILTIN, LOW);
   setHSVColor(_hsvColor);
 }
 
 void LEDStrip::powerOff() {
   _isOn = false;
-  digitalWrite(LED_BUILTIN, HIGH);
   FastLED.showColor(CRGB::Black);
 }
